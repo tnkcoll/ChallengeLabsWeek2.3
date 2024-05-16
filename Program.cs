523566007aea4720229841b9894b5ce94a8b78ee
Ted Collins <theodore.collins2023@outlook.com>
2024-05-16 10:04:28 -0500

    ChallengeLabsWeek2.3

--- Program.cs
+++ Program.cs
@@ -6,30 +6,32 @@
         {
             string userId;
             string userPassword;
+            bool isUserIdMatch;
             bool isPasswordMatch;
-
-            Console.WriteLine("Please enter your User Id.");
-            userId = Console.ReadLine();
 
             for (int i = 3; i > 0; i--)
             {
+                Console.WriteLine("Please enter your User Id.");
+                userId = Console.ReadLine();
+                
                 Console.WriteLine("Please enter your password.");
                 userPassword = Console.ReadLine();
+                isUserIdMatch = MatchUserId(userId);
                 isPasswordMatch = MatchPassword(userPassword);
-                if (isPasswordMatch)
+                if (isUserIdMatch && isPasswordMatch)
                 {
-                    Console.WriteLine("Congratulations! Correct Password.");
+                    Console.WriteLine("Congratulations! Correct UserID and Password.");
                     break;
                 }
                 else
                 {
                     if (i > 1)
                     {
-                        Console.WriteLine("That was the incorrect password. You have " + (i - 1) + " tries left before the system locks you out.");
+                        Console.WriteLine("Either the user Id or password is incorrect. You have " + (i - 1) + " tries left before the system locks you out.");
                     }
                     else
                     {
-                        Console.WriteLine("That was the incorrct password. You have been locked out of the system.");
+                        Console.WriteLine("You have been locked out of the system.");
                     }
                 }
             }
@@ -40,6 +42,18 @@
         {
             const string correctPassword = "password";
             if (password == correctPassword)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool MatchUserId(string user)
+        {
+            const string correctUserId = "Ted";
+            if (user == correctUserId)
             {
                 return true;
             }
